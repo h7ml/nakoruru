c79b1a8e7967a8d6b44cb61fa07fe11b81b7e8e2
h7ml <h7ml@qq.com>
2023-05-08 13:56:13 +0800

    feat: toggle Devtools Visibility

--- src/components/RecoilDevtools/index.tsx
+++ src/components/RecoilDevtools/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { RecoilLogger } from 'recoil-devtools-logger'
 import LogMonitor from 'recoil-devtools-log-monitor'
 import DockMonitor from 'recoil-devtools-dock'
-
+import { Button } from 'antd';
 export const RecoilDevtools = () => {
+  const [isVisible, setIsVisible] = useState(false); // 添加一个状态用于控制 Devtools 的显示与隐藏
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  }
+
   return (
     <>
-      <RecoilLogger />
-      <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q" changeMonitorKey="ctrl-m" defaultIsVisible>
-        <LogMonitor markStateDiff />
-      </DockMonitor>
+      <Button onClick={toggleVisibility}>Toggle Devtools Visibility</Button> {/* 添加一个按钮，用于控制 Devtools 的显示与隐藏 */}
+      {isVisible && ( // 使用条件渲染控制 Devtools 的显示与隐藏
+        <>
+          <RecoilLogger />
+          <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q" changeMonitorKey="ctrl-m" defaultIsVisible>
+            <LogMonitor markStateDiff />
+          </DockMonitor>
+        </>
+      )}
     </>
   )
 }
