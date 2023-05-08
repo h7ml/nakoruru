58bc47433b30939f220f71b3806f4deb7638c3bc
h7ml <h7ml@qq.com>
2023-05-08 23:12:00 +0800

    feat(fetch react-query): react-query

--- src/hooks/react-query/typicode.ts
+++ src/hooks/react-query/typicode.ts
@@ -1,22 +1,23 @@
 /**
  * @author        h7ml <h7ml@qq.com>
  * @date          2023-05-08 21:41:06
- * @lastModified  2023-05-08 21:58:21
+ * @lastModified  2023-05-08 23:11:12
  * Copyright © www.h7ml.cn All rights reserved
  */
-
-import { useMutation, useQuery } from "@tanstack/react-query";
-
 /*
  * @Author: h7ml <h7ml@qq.com>
  * @Date: 2023-05-08 21:41:06
  * @LastEditors: h7ml <h7ml@qq.com>
- * @LastEditTime: 2023-05-08 21:58:16
+ * @LastEditTime: 2023-05-08 23:07:15
  * @FilePath: \reactflow-mind-map\src\hooks\react-query\typicode.ts
  * @Description: 
  * 
  * Copyright (c) 2022 by h7ml<h7ml@qq.com>, All Rights Reserved. 
  */
+
+
+import { useQuery } from "@tanstack/react-query";
+import { queryKeys } from '../query-keys';
 export const UseTypicode = () => {
   type User = {
     id: number;
@@ -25,31 +26,23 @@ export const UseTypicode = () => {
   };
 
   const fetchUsers = async (): Promise<User[]> => {
-    // const { data } = await axios.get('/api/users');
-    return [];
-  };
-
-  const createUser = async (user: User): Promise<User> => {
-    // const { data } = await axios.post('/api/users', user);
-    return [];
+    // 发送请求获取用户数据
+    const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+    const users = await response.json();
+    return users;
   };
 
   const useUsersQuery = () => {
-    return useQuery<User[]>('users', fetchUsers);
+    const ApiResponse = useQuery(<any>{
+      queryKey: queryKeys.userInfo(), // optional, defaults to a string based unique key for the query, e.g. "users" + user.id.to
+      queryFn: fetchUsers, // optional, the function to run to query data, defaults to using the default fetch function. Note: it can take a
+    })
+    console.log('%c [ ApiResponse ]-37', 'font-size:13px; background:pink; color:#bf2c9f;', ApiResponse)
+    return ApiResponse;
   };
 
-  const useCreateUserMutation = () => {
-    return useMutation(createUser, {
-      onSuccess: (data) => {
-        console.log('User created', data);
-      },
-      onError: (error) => {
-        console.error('Failed to create user', error);
-      },
-    });
-  };
+
   return {
     useUsersQuery,
-    useCreateUserMutation
-  }
-}
+  };
+};
--- src/pages/ReactQuery/index.tsx
+++ src/pages/ReactQuery/index.tsx
@@ -1,23 +1,44 @@
 /**
  * @author        h7ml <h7ml@qq.com>
  * @date          2023-05-08 21:59:23
- * @lastModified  2023-05-08 22:00:47
+ * @lastModified  2023-05-08 23:08:28
  * Copyright © www.h7ml.cn All rights reserved
  */
 /*
  * @Author: h7ml <h7ml@qq.com>
  * @Date: 2023-05-08 21:59:23
  * @LastEditors: h7ml <h7ml@qq.com>
- * @LastEditTime: 2023-05-08 22:00:47
+ * @LastEditTime: 2023-05-08 23:08:28
  * @FilePath: \reactflow-mind-map\src\pages\ReactQuery\index.tsx
  * @Description: 
  * 
  * Copyright (c) 2022 by h7ml<h7ml@qq.com>, All Rights Reserved. 
  */
 
+import { UseTypicode } from "@/hooks"
+
 function ReactQuery() {
+  const { useUsersQuery } = UseTypicode();
+
+  const { data, isLoading, isError } = useUsersQuery();
+  console.log('%c [ data ]-24', 'font-size:13px; background:pink; color:#bf2c9f;', data)
+
+  if (isLoading) {
+    return <div>Loading users...</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading users.</div>;
+  }
   return (
-    <div>index</div>
+    <div>
+      {data?.map((user) => (
+        <div key={user.userId}>
+          <p>{user.title}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
   )
 }
 
