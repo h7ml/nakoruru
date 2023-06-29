dc033538649ffb50d266ccb4df08334cf1e43385
h7ml <h7ml@qq.com>
2023-06-29 11:00:23 +0800

    style(lint): 添加femm lint
    添加prettier eslint  lint-staged commitlint husky npmrc nvmrc
    感谢@https://github.com/maomao1996/femm/tree/main/packages/create-initer

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,12 @@
 module.exports = {
+  root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    '@antfu/eslint-config-react',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
