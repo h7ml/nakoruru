ab60203dd28d873cbaee1fd8477173116174d0cf
h7ml <h7ml@qq.com>
2023-06-30 14:15:18 +0800

    feat: lint

--- src/hooks/react-query/ReactFlow/Edges/edge.type.ts
+++ src/hooks/react-query/ReactFlow/Edges/edge.type.ts
@@ -1,18 +1,8 @@
 /**
  * @author        h7ml <h7ml@qq.com>
  * @date          2023-06-30 11:41:10
- * @lastModified  2023-06-30 11:41:12
+ * @lastModified  2023-06-30 14:14:14
  * Copyright © www.h7ml.cn All rights reserved
- */
-/*
- * @Author: h7ml <h7ml@qq.com>
- * @Date: 2023-06-30 11:41:10
- * @LastEditors: h7ml <h7ml@qq.com>
- * @LastEditTime: 2023-06-30 13:57:53
- * @FilePath: /Users/dtstack/Desktop/yunhu/nakoruru/src/hooks/react-query/ReactFlow/Edges/edge.type.ts
- * @Description:
- *
- * Copyright (c) 2022 by h7ml<h7ml@qq.com>, All Rights Reserved.
  */
 export interface EdgeRoot {
   success: boolean
